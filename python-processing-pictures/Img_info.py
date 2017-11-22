a62c120d458e13c5ca84450eb1bdf57e53d52069
hechao <hechao@oceangene.com>
2017-11-22 11:41:35 +0800

    img info

--- python-processing-pictures/Img_info.py
+++ python-processing-pictures/Img_info.py
@@ -10,6 +10,13 @@ def img_info(path):
     return {"max": l, "min": s}
 
 
+def load_img(path):
+    im = Image.open(path)
+    im.show()
+    draw = ImageDraw.getdraw(im)
+    draw.text()
+
+
 def main():
     print(img_info("D:\\img\\59cf49d4f3d93.jpg"))
 
