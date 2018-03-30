1f25c030e381ccb07dfe113758b3ddf2fe8d06f4
superman-wrdh <13419655360@163.com>
2018-03-30 11:08:22 +0800

    df fill NaN

--- data_process/pandas/df_example.py
+++ data_process/pandas/df_example.py
@@ -24,5 +24,28 @@ def df_process():
     print("---------------")
 
 
+def df_fill_null():
+    """
+     过滤空数据
+    """
+    data = [{'A': 1, 'B': 7, 'C': 3},
+            {'A': 2, 'B': None, 'C': 3},
+            {'A': 3, 'B': 2, 'C': 3},
+            {'A': None, 'B': 7, 'C': 3}]
+    df = pd.DataFrame(data=data)  # dict None 变成DataFrame 之后会变成NaN
+
+    df.notnull()
+    df.notna()
+
+    df.isna()
+    df.isnull()
+
+    # 过滤控制
+    df.where(df.notnull(), None)  # 将df里面的 '空NaN' 变成 None
+    df.where(df.notnull(), 'x')   #
+    df.where(pd.notnull(df), 'y')  #
+
+
 if __name__ == '__main__':
-    df_process()
+    # df_process()
+    df_fill_null()
