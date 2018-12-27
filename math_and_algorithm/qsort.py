c7791a659e6018c5b766ff6676db66005dd9dfe1
superman-wrdh <13419655360@163.com>
2018-12-27 23:33:51 +0800

    alo

--- math_and_algorithm/qsort.py
+++ math_and_algorithm/qsort.py
@@ -28,6 +28,10 @@ def qsort2(arr):
 
 
 if __name__ == '__main__':
-    a = [1, 3, 5, 7, 1, 6, 2, 8]
+    a = [1, 3, 5, 7, 9, 1, 6, 2, 2, 8]
+    b = [1, 3, 5, 7, 9, 1, 6, 2, 2, 8]
+    c = [1, 3, 5, 7, 9, 1, 6, 2, 2, 8]
+    d = [1, 3, 5, 7, 9, 1, 6, 2, 2, 8]
+    print(sorted(c))
     print(qsort(a))
-    print(qsort2(a))
+    print(qsort2(d))
